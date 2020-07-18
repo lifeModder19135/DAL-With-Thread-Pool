df5ed794cc6c1c6a9ae55bafe15d5069040f25e9
Nate Tolbert <ntolbertu85@gmail.com>
2020-07-18 01:08:13 -0400

    first commit

---
+++ src/main/java/com/ntolb/DALWithThreadPool/DalWithThreadPoolApplication.java
@@ -0,0 +1,13 @@
+package com.ntolb.DALWithThreadPool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalWithThreadPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalWithThreadPoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntolb/DALWithThreadPool/DalWithThreadPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntolb.DALWithThreadPool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalWithThreadPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
