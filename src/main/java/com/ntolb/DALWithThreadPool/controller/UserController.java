b607fdaf75a43fa0e1a3f01b7dce1738fab1ae00
Nate Tolbert <ntolbertu85@gmail.com>
2020-07-24 10:28:21 -0400

    UserController.java.saveUsers() -- fixed syntax error at annotation parameter -- changed 'MediaType.MULTIPART_FORM_DATA_VALUE' TO {MediaType.MULTIPART_FORM_DATA_VALUE}

--- src/main/java/com/ntolb/DALWithThreadPool/controller/UserController.java
+++ src/main/java/com/ntolb/DALWithThreadPool/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ public class UserController {
 		@Autowired
 		UserService service = new UserService();
 		
-		@PostMapping(value = "/users", consumes="MediaType.MULTIPART_FORM_DATA_VALUE", produces="application/json")
+		@PostMapping(value = "/users", consumes= {MediaType.MULTIPART_FORM_DATA_VALUE}, produces="application/json")
 		public ResponseEntity saveUsers(@RequestParam(value="files") MultipartFile[] files) throws Exception {
 			
 			for(MultipartFile file:files) {
